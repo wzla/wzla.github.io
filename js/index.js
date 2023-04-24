026c98f974e72f2d60ef6ab2b9a2ec220416b3b3
草莓不好吃 <39552860+wzla@users.noreply.github.com>
2023-04-24 16:10:46 +0800

    fetch data from cnode

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-
+fetch("https://cnodejs.org/api/v1/topics").then(res => res.json()).then(res => {
+    res.data.forEach(item => {
+        const $title = document.createElement("h1");
+        $title.textContent = item.title;
+        const $content = document.createElement("div");
+        $content.innerHTML = item.content;
+        document.querySelector("#cnode").append($title);
+        document.querySelector("#cnode").append($content);
+    });
+})
